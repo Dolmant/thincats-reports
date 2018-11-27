44d3ac088f499ab9f81da6b4141b34dd6d379203
Dylan Simmer <goldenoblivion@gmail.com>
2018-11-27 22:48:52 +1100

    added remaining reports

--- main.go
+++ main.go
@@ -15,7 +15,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/gocarina/gocsv"
 	"github.com/robfig/cron"
 	sendgrid "github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
@@ -26,170 +25,6 @@ import (
 
 	"net/http/cookiejar"
 )
-
-// sub part of https://borrower.thincats.com.au/api/loans/2/exchange/balance-transactions
-type BalanceTransaction struct {
-	AccruedInterestAmount     float64 `json:"accruedInterestAmount"`
-	CapitalizedInterestAmount float64 `json:"capitalizedInterestAmount"`
-	FeeAmount                 float64 `json:"feeAmount"`
-	PrincipleAmount           float64 `json:"principleAmount"`
-	RepaymentDueAmount        float64 `json:"repaymentDueAmount"`
-	Total                     float64 `json:"total"`
-	Date                      string  `json:"date"`
-}
-
-// https://borrower.thincats.com.au/api/loans/2/exchange/balance-transactions
-type BalanceTransactions struct {
-	AccruedInterestAmount     float64              `json:"accruedInterestAmount"`
-	CapitalizedInterestAmount float64              `json:"capitalizedInterestAmount"`
-	FeeAmount                 float64              `json:"feeAmount"`
-	PrincipleAmount           float64              `json:"principleAmount"` //the principal remaining
-	RepaymentDueAmount        float64              `json:"repaymentDueAmount"`
-	TotalAmount               float64              `json:"totalAmount"`
-	Transactions              []BalanceTransaction `json:"transactions"`
-}
-
-// https://borrower.thincats.com.au/api/loans/2/exchange/dues
-type Repayment struct {
-	BorrowerInvestorIndicator string  `json:"borrowerInvestorIndicator"`
-	DemandDueStatus           string  `json:"demandDueStatus"`
-	DueAmt                    float64 `json:"dueAmt"`
-	DueType                   string  `json:"dueType"`
-	DueValueDate              string  `json:"dueValueDate"`
-	OrgDueAmt                 float64 `json:"orgDueAmt"`
-	FeePerRepayAmt            float64 `json:"feePerRepayAmt"`
-	IntCapitalizedFlag        string  `json:"intCapitalizedFlag"`
-}
-
-// https://borrower.thincats.com.au/api/loans/2/exchange
-// https://borrower.thincats.com.au/api/loans/2/exchange/current-status
-type Loan struct {
-	Amount                               float64 `json:"amount"` //Original amount
-	Created                              string  `json:"created"`
-	BorrowerRate                         float64 `json:"borrowerRate"`
-	EndDate                              string  `json:"endDate"`
-	LoanTerm                             string  `json:"loanTerm"`
-	LoanTermMonth                        float64 `json:"loanTermMonth"`
-	LoanTermYear                         float64 `json:"loanTermYear"`
-	FirstName                            string  `json:"firstName"`
-	LastName                             string  `json:"lastName"`
-	BorrowerFeePerRepay                  float64 `json:"borrowerFeePerRepay"`
-	BorrowerName                         string  `json:"borrowerName"`
-	LoanAccountID                        string  `json:"loanAccountId"`
-	LoanAcctDueDefaultStatus             string  `json:"loanAcctDueDefaultStatus"`
-	LoanAcctIntCapitalizedFlag           string  `json:"loanAcctIntCapitalizedFlag"`
-	LoanAcctIntCapitalizedFlagInvestor   string  `json:"loanAcctIntCapitalizedFlagInvestor"`
-	LoanAcctStatus                       string  `json:"loanAcctStatus"`
-	LoanAmount                           float64 `json:"loanAmount"`
-	LoanDescription                      string  `json:"loanDescription"`
-	LoanDurationInMonths                 int64   `json:"loanDurationInMonths"`
-	LoanGrade                            string  `json:"loanGrade"`
-	LoanInterestApplicationDay           int64   `json:"loanInterestApplicationDay"`
-	LoanInterestApplicationFrequency     int64   `json:"loanInterestApplicationFrequency"`
-	LoanInterestApplicationFrequencyType string  `json:"loanInterestApplicationFrequencyType"`
-	LoanInvestorRateOfInterest           float64 `json:"loanInvestorRateOfInterest"`
-	LoanLvr                              float64 `json:"loanLvr"`
-	LoanPenalRateOfInterest              float64 `json:"loanPenalRateOfInterest"`
-	LoanProdCode                         string  `json:"loanProdCode"`
-	LoanPurpose                          string  `json:"loanPurpose"`
-	LoanRateOfInterest                   float64 `json:"loanRateOfInterest"`
-	LoanRepaymentMethod                  string  `json:"loanRepaymentMethod"`
-	LoanStartDate                        string  `json:"loanStartDate"`
-	NextIntCalcStartDate                 string  `json:"nextIntCalcStartDate"`
-	SecurityDetails                      string  `json:"securityDetails"`
-	SpdsDocLink                          string  `json:"spdsDocLink"`
-	Version                              int64   `json:"version"`
-	Email                                string  `json:"email"`
-	StartDate                            string  `json:"startDate"`
-	StatusName                           string  `json:"statusName"`
-	Name                                 string  `json:"name"`
-	Id                                   int64   `json:"id"`
-	ExchangeLoanId                       string  `json:"exchangeLoanId"`
-	BalanceTransactions                  BalanceTransactions
-	Repayments                           []Repayment
-}
-
-// https://investorapi.thincats.com.au//loans/loan/blk/invportfolio/10240/1/20
-// https://investorapi.thincats.com.au//loans/loan/exchange/investor/interest/10240/1/20
-// https://investorapi.thincats.com.au//loans/loan/exchange/investor/loans/10240/1/20
-type InvestorLoan struct {
-	LoanAmount                 float64 `json:"loanAmount"`
-	OutstandingPrinciple       float64 `json:"outstandingPrinciple"`
-	RepaymentPaid              string  `json:"repaymentPaid"`
-	RepaymentsToBePaid         string  `json:"repaymentsToBePaid"`
-	NextRepaymentDate          string  `json:"nextRepaymentDate"`
-	BorrowerName               string  `json:"borrowerName"`
-	LoanStartdate              string  `json:"loanStartdate"`
-	LoanEnddate                string  `json:"loanEnddate"`
-	NumofTokensHeld            float64 `json:"numofTokensHeld"`
-	CurrentTokenValue          float64 `json:"currentTokenValue"`
-	OrgTokenValue              float64 `json:"orgTokenValue"`
-	TokenAllocationDate        string  `json:"tokenAllocationDate"`
-	InterestEarned             float64 `json:"interestEarned"`
-	LoanInvestorRateOfInterest float64 `json:"loanInvestorRateOfInterest"`
-	LoanAcctId                 string  `json:"loanAcctId"`
-	InvestorLoan               *Loan
-}
-
-// https://investorapi.thincats.com.au//loans/loan/exchange/10240/txn/1/20
-type Transaction struct {
-	TxnId                     string  `json:"txnId"`
-	TxnValueDate              string  `json:"txnValueDate"`
-	TxnAmt                    float64 `json:"txnAmt"`
-	Txnind                    string  `json:"txnind"`
-	TxnType                   string  `json:"txnType"`
-	TxnDesc                   string  `json:"txnDesc"`
-	AcctId                    string  `json:"acctId"`
-	PartitionKey              string  `json:"partitionKey"`
-	BorrowerInvestorIndicator string  `json:"borrowerInvestorIndicator"`
-}
-
-// investors
-// https://investorapi.thincats.com.au/loans/loan/exchange/investor/balance/10240
-type Investor struct {
-	AccountName      string  `json:"accountName"`
-	Email            string  `json:"email"`
-	GivenName        string  `json:"givenName"`
-	Surname          string  `json:"surname"`
-	Type             string  `json:"type"`
-	AccountBalance   float64 `json:"accountBalance"`
-	BalanceInHold    float64 `json:"balanceInHold"`
-	EffectiveBalance float64 `json:"effectiveBalance"`
-	InterestEarned   float64 `json:"interestEarned"`
-	// This is the ID effectively
-	InvestorAcctName           string         `json:"investorAcctName"`
-	NumOfLoansInvested         float64        `json:"numOfLoansInvested"`
-	NumOfLoansInvestedInClosed float64        `json:"numOfLoansInvestedInClosed"`
-	PrincipalInLiveLoanAccts   float64        `json:"principalInLiveLoanAccts"`
-	PrincipleToRecover         float64        `json:"principleToRecover"`
-	InvestorLoans              []InvestorLoan `json:"investorPortFolioDtoList"` // todo pagination for these transactions
-	Transactions               []Transaction  `json:"transactionLegPageList"`   // todo pagination for these transactions
-}
-
-type Config struct {
-	Email                string
-	Password             string
-	Username             string
-	Pass                 string
-	Basic                string
-	SENDGRID_API_KEY     string
-	Interval             int64
-	SimultaneousRequests int64
-	DetailedMatch        bool
-}
-
-// https://borrower.thincats.com.au/api/loans/loan-managements
-// https://investorapi.thincats.com.au//accounts/accounts
-type Data struct {
-	Config        Config
-	InvestorToken string
-	CookieJar     *cookiejar.Jar
-	Loans         []Loan
-	Investors     []Investor
-	Semaphore     chan int
-	BMutex        *sync.Mutex
-	IMutex        *sync.Mutex
-}
 
 func (data *Data) LoginInvestor() {
 	type Login struct {
@@ -485,112 +320,27 @@ func main() {
 	}))
 
 	authorized.GET("/report/capital-outstanding", func(c *gin.Context) {
-		type CapitalOutstanding struct {
-			LoanId             string `csv:"loan_id"`
-			LoanName           string `csv:"loan_name"`
-			InvestorName       string `csv:"investor_name"`
-			InvestorId         string `csv:"investor_id"`
-			CapitalOutstanding string `csv:"capital_outstanding"`
-			OriginalCapital    string `csv:"original_capital"`
-		}
-
-		CapitalOutstandingTotal := []*CapitalOutstanding{}
-
-		for _, investor := range Data.Investors {
-			for _, loan := range investor.InvestorLoans {
-
-				CapitalOutstandingTotal = append(CapitalOutstandingTotal, &CapitalOutstanding{
-					InvestorId:         investor.AccountName,
-					InvestorName:       investor.GivenName + " " + investor.Surname,
-					LoanId:             loan.LoanAcctId,
-					LoanName:           loan.BorrowerName,
-					CapitalOutstanding: strconv.FormatFloat(float64(loan.CurrentTokenValue*loan.NumofTokensHeld), 'f', 6, 32),
-					OriginalCapital:    strconv.FormatFloat(float64(loan.OrgTokenValue*loan.NumofTokensHeld), 'f', 6, 32),
-				})
-			}
-		}
-
-		csvContent, err := gocsv.MarshalString(&CapitalOutstandingTotal)
-		if err != nil {
-			panic(err)
-		}
 		c.Header("Content-Description", "File Transfer")
 		c.Header("Content-Disposition", "attachment; filename=capital-outstanding-report.csv")
-		c.Data(http.StatusOK, "text/csv", []byte(csvContent))
+		c.Data(http.StatusOK, "text/csv", []byte(CapitalOutstanding(Data)))
 	})
+
 	authorized.GET("/report/investor-balance", func(c *gin.Context) {
-		type InvestorBalance struct {
-			InvestorName             string `csv:"investor_name"`
-			InvestorId               string `csv:"investor_id"`
-			AccountBalance           string `csv:"accountBalance"`
-			BalanceInHold            string `csv:"balanceInHold"`
-			EffectiveBalance         string `csv:"effectiveBalance"`
-			PrincipalInLiveLoanAccts string `csv:"principalInLiveLoanAccts"`
-		}
-
-		InvestorBalanceTotal := []*InvestorBalance{}
-
-		for _, investor := range Data.Investors {
-			InvestorBalanceTotal = append(InvestorBalanceTotal, &InvestorBalance{
-				InvestorId:               investor.AccountName,
-				InvestorName:             investor.GivenName + " " + investor.Surname,
-				AccountBalance:           strconv.FormatFloat(float64(investor.AccountBalance), 'f', 6, 32),
-				BalanceInHold:            strconv.FormatFloat(float64(investor.BalanceInHold), 'f', 6, 32),
-				PrincipalInLiveLoanAccts: strconv.FormatFloat(float64(investor.PrincipalInLiveLoanAccts), 'f', 6, 32),
-				EffectiveBalance:         strconv.FormatFloat(float64(investor.EffectiveBalance), 'f', 6, 32),
-			})
-		}
-
-		csvContent, err := gocsv.MarshalString(&InvestorBalanceTotal)
-		if err != nil {
-			panic(err)
-		}
 		c.Header("Content-Description", "File Transfer")
 		c.Header("Content-Disposition", "attachment; filename=investor-balance-report.csv")
-		c.Data(http.StatusOK, "text/csv", []byte(csvContent))
+		c.Data(http.StatusOK, "text/csv", []byte(MembershipList(Data)))
 	})
+
 	authorized.GET("/report/investor-transactions", func(c *gin.Context) {
-		type InvestorTransactions struct {
-			InvestorName              string `csv:"investor_name"`
-			InvestorId                string `csv:"investor_id"`
-			TxnId                     string `csv:"txnId"`
-			TxnValueDate              string `csv:"txnValueDate"`
-			TxnAmt                    string `csv:"txnAmt"`
-			Txnind                    string `csv:"txnind"`
-			TxnType                   string `csv:"txnType"`
-			TxnDesc                   string `csv:"txnDesc"`
-			AcctId                    string `csv:"acctId"`
-			PartitionKey              string `csv:"partitionKey"`
-			BorrowerInvestorIndicator string `csv:"borrowerInvestorIndicator"`
-		}
-
-		InvestorTransactionsTotal := []*InvestorTransactions{}
-
-		for _, investor := range Data.Investors {
-			for _, transaction := range investor.Transactions {
-				InvestorTransactionsTotal = append(InvestorTransactionsTotal, &InvestorTransactions{
-					InvestorId:                investor.AccountName,
-					InvestorName:              investor.GivenName + " " + investor.Surname,
-					TxnId:                     transaction.TxnId,
-					TxnValueDate:              transaction.TxnValueDate,
-					TxnAmt:                    strconv.FormatFloat(float64(transaction.TxnAmt), 'f', 6, 32),
-					Txnind:                    transaction.Txnind,
-					TxnType:                   transaction.TxnType,
-					TxnDesc:                   transaction.TxnDesc,
-					AcctId:                    transaction.AcctId,
-					PartitionKey:              transaction.PartitionKey,
-					BorrowerInvestorIndicator: transaction.BorrowerInvestorIndicator,
-				})
-			}
-		}
-
-		csvContent, err := gocsv.MarshalString(&InvestorTransactionsTotal)
-		if err != nil {
-			panic(err)
-		}
 		c.Header("Content-Description", "File Transfer")
 		c.Header("Content-Disposition", "attachment; filename=investor-transaction-report.csv")
-		c.Data(http.StatusOK, "text/csv", []byte(csvContent))
+		c.Data(http.StatusOK, "text/csv", []byte(UserTransactions(Data)))
+	})
+
+	authorized.GET("/report/loan-transactions", func(c *gin.Context) {
+		c.Header("Content-Description", "File Transfer")
+		c.Header("Content-Disposition", "attachment; filename=loan-transaction-report.csv")
+		c.Data(http.StatusOK, "text/csv", []byte(LoanTransactions(Data)))
 	})
 
 	authorized.GET("/report/membership-list", func(c *gin.Context) {
@@ -609,6 +359,12 @@ func main() {
 		c.Header("Content-Description", "File Transfer")
 		c.Header("Content-Disposition", "attachment; filename=MostRecentBidListing-"+time.Now().Format("YYYY-MM-DD")+".csv")
 		c.Data(http.StatusOK, "text/csv", []byte(BidListing(Data)))
+	})
+
+	authorized.GET("/report/loan-loss", func(c *gin.Context) {
+		c.Header("Content-Description", "File Transfer")
+		c.Header("Content-Disposition", "attachment; filename=LoanLoss-"+time.Now().Format("YYYY-MM-DD")+".csv")
+		c.Data(http.StatusOK, "text/csv", []byte(LoanLoss(Data)))
 	})
 
 	authorized.GET("/refresh", func(c *gin.Context) {
--- reports.go
+++ reports.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"strconv"
+	"time"
 
 	"github.com/gocarina/gocsv"
 )
 
+// Bid Listing gets the morst recent bid listing for each loan
 func BidListing(data Data) string {
 	csvTitles := "ID,Name,"
 	csvBody := ""
@@ -32,6 +34,7 @@ func BidListing(data Data) string {
 	return csvTitles + csvBody
 }
 
+// LenderSummary gets the lender holdings, the total bids on each loan per investor
 func LenderSummary(data Data) string {
 	csvTitles := "ID,Name,"
 	csvBody := ""
@@ -112,6 +115,7 @@ func LenderSummary(data Data) string {
 	return csvTitles + csvInternal + csvInternalp + csvExternal + csvExternalp + csvTotal + csvBidCount + csvAvgBids + csvBody
 }
 
+//MembershipList gets the balances for each lender
 func MembershipList(data Data) string {
 	type InvestorBalance struct {
 		InvestorName             string `csv:"Investor Name"`
@@ -136,6 +140,163 @@ func MembershipList(data Data) string {
 	}
 
 	csvContent, err := gocsv.MarshalString(&InvestorBalanceTotal)
+	if err != nil {
+		panic(err)
+	}
+	return csvContent
+}
+
+// CapitalOutstanding gets said property for each loan
+func CapitalOutstanding(data Data) string {
+	type CapitalOutstanding struct {
+		LoanId             string `csv:"loan_id"`
+		LoanName           string `csv:"loan_name"`
+		InvestorName       string `csv:"investor_name"`
+		InvestorId         string `csv:"investor_id"`
+		CapitalOutstanding string `csv:"capital_outstanding"`
+		OriginalCapital    string `csv:"original_capital"`
+	}
+
+	CapitalOutstandingTotal := []*CapitalOutstanding{}
+
+	for _, investor := range data.Investors {
+		for _, loan := range investor.InvestorLoans {
+
+			CapitalOutstandingTotal = append(CapitalOutstandingTotal, &CapitalOutstanding{
+				InvestorId:         investor.AccountName,
+				InvestorName:       investor.GivenName + " " + investor.Surname,
+				LoanId:             loan.LoanAcctId,
+				LoanName:           loan.BorrowerName,
+				CapitalOutstanding: strconv.FormatFloat(float64(loan.CurrentTokenValue*loan.NumofTokensHeld), 'f', 6, 32),
+				OriginalCapital:    strconv.FormatFloat(float64(loan.OrgTokenValue*loan.NumofTokensHeld), 'f', 6, 32),
+			})
+		}
+	}
+
+	csvContent, err := gocsv.MarshalString(&CapitalOutstandingTotal)
+	if err != nil {
+		panic(err)
+	}
+	return csvContent
+}
+
+// LoanLoss calculates the loan losses for each loan
+// todo add in year on year losses
+func LoanLoss(data Data) string {
+	// todo add in year by year returns and losses - might require trawling the transactions
+	type LoanPaymentSummary struct {
+		LoanName           string `csv:"Loan Name"`
+		LoanID             string `csv:"Loan ID"`
+		DueAmount          string `csv:"Due Amount"`
+		OriginalAmount     string `csv:"Original Amount"`
+		CapitalOutstanding string `csv:"Principal Outstanding"`
+	}
+
+	LoanPaymentSummaryTotal := []*LoanPaymentSummary{}
+
+	for _, loan := range data.Loans {
+		dueTotal := float64(0)
+		overDue := false
+		for _, due := range loan.Repayments {
+			dueTotal += due.DueAmt
+			if due.DueValueDate < time.Now().Add(-time.Hour*24*90).Format("2006-01-02") {
+				overDue = true
+			}
+		}
+		if overDue {
+			LoanPaymentSummaryTotal = append(LoanPaymentSummaryTotal, &LoanPaymentSummary{
+				LoanName:           loan.Name,
+				LoanID:             loan.LoanAccountID,
+				DueAmount:          strconv.FormatFloat(dueTotal, 'f', 6, 32),
+				OriginalAmount:     strconv.FormatFloat(loan.Amount, 'f', 6, 32),
+				CapitalOutstanding: strconv.FormatFloat(loan.BalanceTransactions.PrincipleAmount, 'f', 6, 32),
+			})
+		}
+	}
+
+	csvContent, err := gocsv.MarshalString(&LoanPaymentSummaryTotal)
+	if err != nil {
+		panic(err)
+	}
+	return csvContent
+}
+
+// LoanTransactions has all loan based transactions
+func LoanTransactions(data Data) string {
+	type LoanTransactions struct {
+		LoanName                  string `csv:"loan_name"`
+		LoanID                    string `csv:"loan_id"`
+		AccruedInterestAmount     string `csv:"accruedInterestAmount"`
+		CapitalizedInterestAmount string `csv:"capitalizedInterestAmount"`
+		FeeAmount                 string `csv:"feeAmount"`
+		PrincipleAmount           string `csv:"principleAmount"`
+		RepaymentDueAmount        string `csv:"repaymentDueAmount"`
+		Total                     string `csv:"total"`
+		Date                      string `csv:"date"`
+	}
+
+	LoanTransactionsAll := []*LoanTransactions{}
+
+	for _, loan := range data.Loans {
+		for _, transaction := range loan.BalanceTransactions.Transactions {
+			LoanTransactionsAll = append(LoanTransactionsAll, &LoanTransactions{
+				LoanName:                  loan.Name,
+				LoanID:                    loan.LoanAccountID,
+				AccruedInterestAmount:     strconv.FormatFloat(transaction.AccruedInterestAmount, 'f', 6, 32),
+				CapitalizedInterestAmount: strconv.FormatFloat(transaction.CapitalizedInterestAmount, 'f', 6, 32),
+				FeeAmount:                 strconv.FormatFloat(transaction.FeeAmount, 'f', 6, 32),
+				PrincipleAmount:           strconv.FormatFloat(transaction.PrincipleAmount, 'f', 6, 32),
+				RepaymentDueAmount:        strconv.FormatFloat(transaction.RepaymentDueAmount, 'f', 6, 32),
+				Total:                     strconv.FormatFloat(transaction.Total, 'f', 6, 32),
+				Date:                      transaction.Date,
+			})
+		}
+	}
+
+	csvContent, err := gocsv.MarshalString(&LoanTransactionsAll)
+	if err != nil {
+		panic(err)
+	}
+	return csvContent
+}
+
+// UserTransactions is all user based transactions
+func UserTransactions(data Data) string {
+	type InvestorTransactions struct {
+		InvestorName              string `csv:"investor_name"`
+		InvestorId                string `csv:"investor_id"`
+		TxnId                     string `csv:"txnId"`
+		TxnValueDate              string `csv:"txnValueDate"`
+		TxnAmt                    string `csv:"txnAmt"`
+		Txnind                    string `csv:"txnind"`
+		TxnType                   string `csv:"txnType"`
+		TxnDesc                   string `csv:"txnDesc"`
+		AcctId                    string `csv:"acctId"`
+		PartitionKey              string `csv:"partitionKey"`
+		BorrowerInvestorIndicator string `csv:"borrowerInvestorIndicator"`
+	}
+
+	InvestorTransactionsTotal := []*InvestorTransactions{}
+
+	for _, investor := range data.Investors {
+		for _, transaction := range investor.Transactions {
+			InvestorTransactionsTotal = append(InvestorTransactionsTotal, &InvestorTransactions{
+				InvestorId:                investor.AccountName,
+				InvestorName:              investor.GivenName + " " + investor.Surname,
+				TxnId:                     transaction.TxnId,
+				TxnValueDate:              transaction.TxnValueDate,
+				TxnAmt:                    strconv.FormatFloat(float64(transaction.TxnAmt), 'f', 6, 32),
+				Txnind:                    transaction.Txnind,
+				TxnType:                   transaction.TxnType,
+				TxnDesc:                   transaction.TxnDesc,
+				AcctId:                    transaction.AcctId,
+				PartitionKey:              transaction.PartitionKey,
+				BorrowerInvestorIndicator: transaction.BorrowerInvestorIndicator,
+			})
+		}
+	}
+
+	csvContent, err := gocsv.MarshalString(&InvestorTransactionsTotal)
 	if err != nil {
 		panic(err)
 	}
